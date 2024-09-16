df8f8d41a5710eb7ade4c7efc8faf9b8d3aaa33e
AJ1732 <ejemeniboi@gmail.com>
2024-09-16 18:57:18 +0100

    chore: structure

--- frontend/src/pages/Community.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Community() {
-  return (
-    <div className='mt-20'>Community</div>
-  )
-}
-
-export default Community
---
+++ frontend/src/pages/Community.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Community() {
+  return (
+    <div className="mt-20 max-w-7xl mx-auto space-y-12">
+      <header>Header</header>
+
+      <figure className="h-[510px] bg-purple-400"></figure>
+
+      <section>
+        <div>First Div</div>
+      </section>
+    </div>
+  );
+}
+
+export default Community;
