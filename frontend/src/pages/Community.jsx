d407f5bd23c74a11d8151a44d2a7556c7f927b4d
AJ1732 <ejemeniboi@gmail.com>
2024-09-16 19:33:22 +0100

    chore: update

--- frontend/src/pages/Community.jsx
+++ frontend/src/pages/Community.jsx
@@ -1,21 +1,29 @@
 import React from "react";
+import roomImg1 from "../components/assets/CommunityPage/Frame 32.png";
+import roomImg2 from "../components/assets/CommunityPage/Frame 33.png";
+import roomImg3 from "../components/assets/CommunityPage/Frame 34.png";
+import roomImg4 from "../components/assets/CommunityPage/Frame 35.png";
 
 function Community() {
   return (
     <div className="mt-20 max-w-7xl mx-auto space-y-12 py-12">
       <header className="my-20">
-        <h2 className="text-black font-bold text-[40px] leading-10">Community Page</h2>
+        <h2 className="text-black font-bold text-[40px] leading-10">
+          Community Page
+        </h2>
       </header>
 
       <figure className="h-[510px] bg-purple-400"></figure>
 
       <section className="flex flex-col divide-y divide-black/20">
-        {[1, 2, 3, 4].map((item) => (
+        {[roomImg3, roomImg2, roomImg1, roomImg4].map((item, index) => (
           <div
-            key={item}
+            key={`room-${index}`}
             className="py-6 flex justify-start items-center gap-10"
           >
-            <figure className="min-w-48 aspect-square bg-purple-300"></figure>
+            <figure className="min-w-48 aspect-square bg-purple-300">
+              <img src={item} alt="" className="size-full object-cover" />
+            </figure>
 
             <div className="space-y-7">
               <h3 className="text-3xl">Placeholder Title</h3>
