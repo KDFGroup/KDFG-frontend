13ea7623abbc35a931c697882c7d0aa1f4fdcc01
glowwEE <gloryokafor5@gmail.com>
2024-09-16 19:21:01 +0100

    add: added images  in a new folder called comunityPage located  at assets folder

--- frontend/src/pages/Community.jsx
+++ frontend/src/pages/Community.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Community() {
   return (
     <div className="mt-20 max-w-7xl mx-auto space-y-12">
-      <header>Header</header>
+      <header className="my-20">
+        <h2 className="text-black font-bold text-[40px] leading-10">Community Page</h2>
+      </header>
 
       <figure className="h-[510px] bg-purple-400"></figure>
 
